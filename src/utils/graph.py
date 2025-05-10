c4a6cb30e4762aa5d7813fb5f8ac8869d44413c9
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-05-10 15:31:36 +0900

    update grapf

--- src/utils/graph.py
+++ src/utils/graph.py
@@ -1,6 +1,6 @@
 import numpy as np
+from numba import njit, prange, typed
 import networkx as nx
-from numba import njit, prange
 
 @njit(parallel=True)
 def polar_to_cartesian(scan_data):
@@ -9,12 +9,13 @@ def polar_to_cartesian(scan_data):
     y_coords = scan_data * np.sin(angles)
     return x_coords, y_coords
 
-
 @njit(parallel=True)
 def build_edges(x_coords, y_coords, max_distance):
     num_points = len(x_coords)
-    edges = []
-
+    
+    # Numba対応のリスト型に変更
+    edges = typed.List()
+    
     for i in prange(num_points):
         for j in range(i + 1, num_points):
             dist = np.sqrt((x_coords[i] - x_coords[j]) ** 2 + (y_coords[i] - y_coords[j]) ** 2)
@@ -22,7 +23,6 @@ def build_edges(x_coords, y_coords, max_distance):
                 edges.append((i, j))
     
     return edges
-
 
 def lidar_to_graph(scan_data, max_distance=1.0):
     x_coords, y_coords = polar_to_cartesian(scan_data)
